7bd5215bd9b2c9cd560dc956dbd124fd1add8b0b
unknown <lucas.olisboa@outlook.com>
2022-07-12 21:15:20 -0300

    implementing google autocomplete library and navigation

--- App.js
+++ App.js
@@ -1,19 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+
+      <NavigationContainer>
+      <SafeAreaProvider>
+
+        <Stack.Navigator>
+          <Stack.Screen
+          name='HomeScreen'
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+            }}
+            />
+            <Stack.Screen
+          name='MapScreen'
+          component={MapScreen}
+          options={{
+            headerShown: false,
+            }}
+            />
+        </Stack.Navigator>
+      
+      </SafeAreaProvider>
+      </NavigationContainer>
+      
+      
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/MapScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+
+export default MapScreen
+const styles = StyleSheet.create({})
